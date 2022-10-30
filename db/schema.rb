0e0625a5355e55b2d4fec435c7bea4a081ce539e
Prabhakar Poudel <20899846+Prabhakar-Poudel@users.noreply.github.com>
2022-10-31 00:01:07 +0100

    more fix

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2022_10_28_182851) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "slots", force: :cascade do |t|
     t.datetime "starts_at", null: false
     t.datetime "ends_at", null: false
